838410cb6f94ff53961904e6ac1b5172ac44429e
Risiamu <kousakiaya@icloud.com>
2025-03-23 15:30:11 +0100

    Convert vue.config.js to vue.config.ts using Vite setup
    Migrated the configuration file to TypeScript and updated the export format to align with Vite's structure. This includes introducing `defineConfig`, updating the `base` property, and setting up placeholders for plugins and aliases.

---
+++ vue.config.ts
@@ -0,0 +1,12 @@
+// module.exports = {
+//   publicPath: '/Risi-s-Vue-Blog/'
+// }
+export default defineConfig({
+  base: "/Risi-s-Vue-Blog/",
+  plugins: [],
+  resolve: {
+    alias: [
+
+    ]
+  },
+})
